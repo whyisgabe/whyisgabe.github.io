d674f98befb711e46d85aa235d1b600972a35af8
whyisgabe <82547013+whyisgabe@users.noreply.github.com>
2021-04-28 15:02:06 -0600

    Update scripts.js

--- misc/scripts.js
+++ misc/scripts.js
@@ -1,3 +1,6 @@
 function newestProject() {
   window.location = "https://whyisgabe.github.io/projects/4-30-21_geo_country";
 }
+
+var newestproject = "Western Geography Country Project";
+document.GetElementById("newprojectbanner").innerHTML = "Check out my newest project: " + newestproject;
